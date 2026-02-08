fc24c1b7b27fe806da6bd752ea66add0abe4ba93
ΜΑΡΙΛΙΑ ΣΤΑΥΡΙΑΝΑΚΟΥ <mariliastaurianakou@192.168.68.123>
2026-02-09 01:36:10 +0200

    fix upload route

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server'
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST() {
-  return NextResponse.json({ success: true })
+export async function POST(req: NextRequest) {
+  return NextResponse.json({ success: true });
 }
